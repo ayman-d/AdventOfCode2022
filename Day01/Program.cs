af067a10198145e97160282201423369dca92a2e
Ayman Al-Dali <aymand1982@gmail.com>
2022-12-02 01:13:14 -0700

    First addition of code. Day 1 and 2 done

---
+++ Day01/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Day01
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      string input = File.ReadAllText("../../../input.txt");
+
+      SolveProblemA();
+      SolveProblemB();
+
+      void SolveProblemA()
+      {
+        var solution1 = input.Split(Environment.NewLine+ Environment.NewLine)
+          .Select(group => group.Split(Environment.NewLine).Select(calory => int.Parse(calory)).Sum()).Max();
+
+        Console.WriteLine(solution1);
+      }
+
+      void SolveProblemB()
+      {
+        var solution2 = input.Split(Environment.NewLine+Environment.NewLine)
+          .Select(group => group.Split(Environment.NewLine).Select(calory => int.Parse(calory)).Sum()).OrderByDescending(calory => calory).Take(3).Sum();
+
+        Console.WriteLine(solution2);
+      }
+    }
+  }
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Day02
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      string input = File.ReadAllText("../../../input.txt");
+
+      SolveProblemA();
+      SolveProblemB();
+
+      void SolveProblemA()
+      {
+        int score = 0;
+        var games = input.Split(Environment.NewLine);
+
+        foreach (var game in games)
+        {
+          int opponentPlay = game.Split(" ")[0] switch
+          {
+            "A" => 1,
+            "B" => 2,
+            "C" => 3,
+            _ => 0
+          };
+
+          int myPlay = game.Split(" ")[1] switch
+          {
+            "X" => 1,
+            "Y" => 2,
+            "Z" => 3,
+            _ => 0
+          };
+
+          score +=
+            myPlay == opponentPlay ? myPlay + 3 :
+            myPlay - opponentPlay == 1 || (myPlay == 1 && opponentPlay == 3) ? myPlay + 6 :
+            myPlay;
+        }
+
+        Console.WriteLine(score);
+      }
+
+      void SolveProblemB()
+      {
+        int score = 0;
+        var games = input.Split(Environment.NewLine);
+
+        foreach (var game in games)
+        {
+          int opponentPlay = game.Split(" ")[0] switch
+          {
+            "A" => 1,
+            "B" => 2,
+            "C" => 3,
+            _ => 0
+          };
+
+          int myPlay = game.Split(" ")[1] switch
+          {
+            "X" => opponentPlay == 1 ? 3 : opponentPlay - 1,
+            "Y" => opponentPlay,
+            "Z" => opponentPlay == 3 ? 1 : opponentPlay + 1,
+            _ => 0
+          };
+
+          score +=
+            myPlay == opponentPlay ? myPlay + 3 :
+            myPlay - opponentPlay == 1 || (myPlay == 1 && opponentPlay == 3) ? myPlay + 6 :
+            myPlay;
+        }
+
+        Console.WriteLine(score);
+      }
+    }
+  }
+}

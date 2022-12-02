dcad5f71c01fb45e3d814e1f4a817a7a29000598
Ayman Al-Dali <aymand1982@gmail.com>
2022-12-02 01:16:49 -0700

    Added prep for Day03

---
+++ Day03/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Day03
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      string input = File.ReadAllText("../../../input.txt");
+
+      SolveProblemA();
+      SolveProblemB();
+
+      void SolveProblemA()
+      {
+        Console.WriteLine("hi A");
+      }
+
+      void SolveProblemB()
+      {
+        Console.WriteLine("hi B");
+      }
+    }
+  }
+}

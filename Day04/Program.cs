526d10f978cd0fff8e898300827adf7ad30f579e
Ayman Al-Dali <aymand1982@gmail.com>
2022-12-03 23:26:39 -0700

    Solved day 4. Added the remaining project placeholders

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -8,15 +8,103 @@
 
       SolveProblemA();
       SolveProblemB();
+      AltSolveProblemA();
+      AltSolveProblemB();
 
       void SolveProblemA()
       {
-        Console.WriteLine("hi A");
+        var assignments = input.Split(Environment.NewLine);
+
+        int counter = 0;
+
+        foreach (var assignment in assignments)
+        {
+          var left = assignment.Split(",")[0].Split("-").Select(x => int.Parse(x)).ToList();
+          var right = assignment.Split(",")[1].Split("-").Select(x => int.Parse(x)).ToList();
+
+          if ((left.First() >= right.First() && left.Last() <= right.Last()) || (right.First() >= left.First() && right.Last() <= left.Last()))
+          {
+            counter++;
+          }
+        }
+
+        //602
+        Console.WriteLine(counter);
       }
 
       void SolveProblemB()
       {
-        Console.WriteLine("hi B");
+        var assignments = input.Split(Environment.NewLine);
+
+        int counter = 0;
+
+        foreach (var assignment in assignments)
+        {
+          var left = assignment.Split(",")[0].Split("-").Select(x => int.Parse(x)).ToList();
+          var right = assignment.Split(",")[1].Split("-").Select(x => int.Parse(x)).ToList();
+
+          if (!(left.First() > right.Last() || left.Last() < right.First()))
+          {
+            counter++;
+          }
+        }
+
+        //891
+        Console.WriteLine(counter);
+      }
+
+      // =============================
+      // alternatives (With HashSet<>)
+      // =============================
+
+      void AltSolveProblemA()
+      {
+        var assignments = input.Split(Environment.NewLine);
+
+        int counter = 0;
+
+        foreach (var assignment in assignments)
+        {
+          var leftFirst = int.Parse(assignment.Split(",")[0].Split("-")[0]);
+          var leftSecond = int.Parse(assignment.Split(",")[0].Split("-")[1]);
+          var rightFirst = int.Parse(assignment.Split(",")[1].Split("-")[0]);
+          var rightSecond = int.Parse(assignment.Split(",")[1].Split("-")[1]);
+          HashSet<int> left = new HashSet<int>(Enumerable.Range(leftFirst, leftSecond - leftFirst + 1));
+          HashSet<int> right = new HashSet<int>(Enumerable.Range(rightFirst, rightSecond - rightFirst + 1));
+
+          if (left.IsSubsetOf(right) || right.IsSubsetOf(left))
+          {
+            counter++;
+          }
+        }
+
+        //602
+        Console.WriteLine(counter);
+      }
+
+      void AltSolveProblemB()
+      {
+        var assignments = input.Split(Environment.NewLine);
+
+        int counter = 0;
+
+        foreach (var assignment in assignments)
+        {
+          var leftFirst = int.Parse(assignment.Split(",")[0].Split("-")[0]);
+          var leftSecond = int.Parse(assignment.Split(",")[0].Split("-")[1]);
+          var rightFirst = int.Parse(assignment.Split(",")[1].Split("-")[0]);
+          var rightSecond = int.Parse(assignment.Split(",")[1].Split("-")[1]);
+          HashSet<int> left = new HashSet<int>(Enumerable.Range(leftFirst, leftSecond - leftFirst + 1));
+          HashSet<int> right = new HashSet<int>(Enumerable.Range(rightFirst, rightSecond - rightFirst + 1));
+
+          if (left.Intersect(right).Any())
+          {
+            counter++;
+          }
+        }
+
+        //891
+        Console.WriteLine(counter);
       }
     }
   }
---
+++ Day12/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Day05
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      string input = File.ReadAllText("../../../input.txt");
+
+      SolveProblemA();
+      SolveProblemB();
+
+      void SolveProblemA()
+      {
+        Console.WriteLine("hi A");
+      }
+
+      void SolveProblemB()
+      {
+        Console.WriteLine("hi B");
+      }
+    }
+  }
+}
